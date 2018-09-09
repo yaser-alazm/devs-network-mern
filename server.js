3134e3f0bef5f6c3707a6e755e5e9d2cb14585ff
yaser-alazem <yaser.azem@hotmail.com>
2018-09-10 00:57:09 +0300

    setting useNewUrlParser to true and adding User model

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const db = require("./config/keys").mongoURI;
 //MongoDB connect
 
 mongoose
-  .connect(db)
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
