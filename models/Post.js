b1e57e150fdaf484b268c405e8bbf78e04525032
yaser-alazem <yaser.azem@hotmail.com>
2018-09-13 14:41:08 +0300

    adding post model

---
+++ models/Post.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  text: {
+    type: String,
+    required: true
+  }
+});
