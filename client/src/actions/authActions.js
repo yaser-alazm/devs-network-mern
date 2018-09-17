0de8a50d34fca91f88da26ae0149634494350b2a
yaser-alazem <yaser.azem@hotmail.com>
2018-09-17 15:14:40 +0300

    setting up registeration with redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,10 +1,49 @@
-import { TEST_DISPATCH } from "./types";
+import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 //Register user
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post("/api/users/register", userData)
+    .then(res => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
 
-export const registerUser = userData => {
+//Login - Get User Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      //Save token to localStorage
+      const { token } = res.data;
+      //Set token to the LS
+      localStorage.setItem("jwtToken", token);
+      //Set token to auth header
+      setAuthToken(token);
+      //Decode token to get user data
+      const decode = jwt_decode(token);
+      //Set current user
+      dispatch(setCurrentUser(decode));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+export const setCurrentUser = decode => {
   return {
-    type: TEST_DISPATCH,
-    payload: userData
+    type: SET_CURRENT_USER,
+    payload: decode
   };
 };
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import axios from "axios";
+import { withRouter } from "react-router-dom"; //Ability to use redirect from authActions
 import classnames from "classnames";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
@@ -20,6 +20,16 @@ class Register extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  //Lifecycle methde runs after the component receives any props
+  componentWillReceiveProps(nextProps) {
+    //Setting errors props comming from redux into the errors state of the component
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
+
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
@@ -34,11 +44,7 @@ class Register extends Component {
       password2: this.state.password2
     };
 
-    this.props.registerUser(newUser);
-    // axios
-    //   .post("/api/users/register", newUser)
-    //   .then(res => console.log(res.data))
-    //   .catch(err => this.setState({ errors: err.response.data }));
+    this.props.registerUser(newUser, this.props.history);
   }
 
   render() {
@@ -129,14 +135,16 @@ class Register extends Component {
 
 Register.propTypes = {
   registerUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  auth: state.auth
+  auth: state.auth,
+  errors: state.errors
 });
 
 export default connect(
   mapStateToProps,
   { registerUser }
-)(Register);
+)(withRouter(Register));
